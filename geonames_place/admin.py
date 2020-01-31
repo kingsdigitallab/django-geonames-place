4b6960f784a08d4985489c8792d225e5944df004
Miguel Vieira <jmvieira@gmail.com>
2020-01-31 14:42:41 +0000

    👌 Improve: Autocomple from geonames.

--- geonames_place/admin.py
+++ geonames_place/admin.py
@@ -1,26 +1,38 @@
 from django.contrib import admin
-from geonames_place.models import (ClassDescription, Country, FeatureClass,
-                                   Place)
+from geonames_place.models import ClassDescription, Country, FeatureClass, Place
 
 
 @admin.register(ClassDescription)
 class ClassDescriptionAdmin(admin.ModelAdmin):
-    search_fields = ['title']
+    search_fields = ["title"]
 
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'code']
+    search_fields = ["name", "code"]
 
 
 @admin.register(FeatureClass)
 class FeatureClassAdmin(admin.ModelAdmin):
-    search_fields = ['title']
+    search_fields = ["title"]
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    autocomplete_fields = ['class_description', 'country', 'feature_class']
-    list_display = ['address', 'class_description', 'country']
-    list_filter = ['class_description', 'country']
-    search_fields = ['address', 'country__name', 'country__code']
+    autocomplete_fields = ["class_description", "country", "feature_class"]
+    list_display = ["address", "class_description", "country"]
+    list_filter = ["class_description", "country"]
+    search_fields = ["address", "country__name", "country__code"]
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(
+            request, queryset, search_term
+        )
+
+        if len(queryset) < 3 and len(search_term) > 3:
+            Place.create_or_update_from_geonames(search_term)
+            queryset, use_distinct = super().get_search_results(
+                request, queryset, search_term
+            )
+
+        return queryset, use_distinct
